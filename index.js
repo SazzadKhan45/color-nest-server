e74b2fde106bea454b18612f7fba09f62b9e5f62
Sazzadkhan45 <sazzadkhan753@gmail.com>
2025-11-08 23:32:55 +0600

    sucessfuly data post mongodb databse

--- index.js
+++ index.js
@@ -25,18 +25,35 @@ async function run() {
   try {
     // Connect the client to the server
     await client.connect();
+
+    const db = client.db("Art_works");
+    const artsCollection = db.collection("arts");
+    const userCollection = db.collection("user");
+
+    // POST API
+    app.post("/test", async (req, res) => {
+      try {
+        const data = req.body; // get data from frontend
+        const result = await artsCollection.insertOne(data);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: "Error inserting data" });
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
+
+    //
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-//
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
